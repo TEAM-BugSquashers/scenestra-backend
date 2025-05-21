456ef12aefa0ae30d2254f199998ff661a1c85f7
hyunbin0210 <58248714+hyunbin0210@users.noreply.github.com>
2025-05-22 08:49:54 +0900

    fix: Movie.java에 open_date의 타입을 DateTime에서 Date로 수정 및 직렬화 방지를 위한 @JsonIgnoreProperties 어노테이션 추가 #11, fix: 전체 영화목록 출력시 직렬화 오류 해결 #11

--- src/main/java/com/bugsquashers/backend/movie/domain/Movie.java
+++ src/main/java/com/bugsquashers/backend/movie/domain/Movie.java
@@ -1,11 +1,11 @@
 package com.bugsquashers.backend.movie.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonIgnoreProperties("movieGenres")
 public class Movie {
     @Id
     private String movieId;
@@ -25,7 +26,8 @@ public class Movie {
 
     private String director;
 
-    private LocalDateTime openDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date openDate;
 
     private Integer numAudience;
 
