6bb74d9812a6dadf05a343aaea1e0880d8fbcc25
hyunbin0210 <58248714+hyunbin0210@users.noreply.github.com>
2025-06-04 10:30:42 +0900

    fix: RequestMapping 수정, 설명 수정 #54

--- src/main/java/com/bugsquashers/backend/review/ReviewController.java
+++ src/main/java/com/bugsquashers/backend/review/ReviewController.java
@@ -15,20 +15,21 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/review")
+@RequestMapping("/api/reviews")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
 
     // 글 쓰기
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "리뷰 작성", description = "리뷰를 작성합니다.")
     public ApiResponse<ReviewResponse> createReview(@ModelAttribute ReviewRequest reviewRequest) {
         return ApiResponse.onSuccess(SuccessStatus.OK, reviewService.createReview(reviewRequest)).getBody();
     }
 
     // 전체 리뷰 목록
     @GetMapping
-    @Operation(summary = "전체 리뷰 목록(상영관 구별X)", description = "전체 리뷰 목록을 조회합니다.")
+    @Operation(summary = "전체 리뷰 목록(상영관 구별X) 조회", description = "전체 리뷰 목록을 조회합니다.")
     public ResponseEntity<ApiResponse<Object>> getAllReviews() {
         return ApiResponse.onSuccess(SuccessStatus.OK, reviewService.getAllReview());
     }
@@ -42,7 +43,7 @@ public class ReviewController {
 
     // 상영관 별 리뷰 목록
     @GetMapping("/theater/{theaterId}")
-    @Operation(summary = "상영관 별 리뷰 목록(theaterId 이용)", description = "상영관 별 리뷰 목록을 조회합니다.")
+    @Operation(summary = "상영관 별 리뷰 목록(theaterId 이용) 조회", description = "상영관 별 리뷰 목록을 조회합니다.")
     public List<ReviewListResponse> getReviewsByTheater(@PathVariable Integer theaterId) {
         return reviewService.getReviewByTheaterId(theaterId);
     }
